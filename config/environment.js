d440ad72120a616d500d68eb31b87f7a74460d41
Bestie Joel <bestiejoel@gmail.com>
2020-08-17 21:52:01 -0300

    add ember ifa and root-url for gh-pages image fixes

--- config/environment.js
+++ config/environment.js
@@ -22,6 +22,11 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    ifa: {
+      enabled: true,
+      inline: true,
     }
   };
 
